ddc9120627dc7d19daca849204da854aefcc48ad
crazyqin <crazyqin@users.noreply.github.com>
2026-03-14 03:27:49 +0800

    fix: 修复 tags 和 office 模块测试问题
    - tags: 修复事务处理中的 defer 问题
    - office: 添加 Close 方法停止 goroutine

--- internal/tags/manager.go
+++ internal/tags/manager.go
@@ -438,7 +438,9 @@ func (m *Manager) SetFileTags(filePath string, tagIDs []string) error {
 	if err != nil {
 		return err
 	}
-	_ = tx.Rollback()
+	defer func() {
+		_ = tx.Rollback()
+	}()
 
 	// 删除现有标签
 	_, err = tx.Exec("DELETE FROM file_tags WHERE file_path = ?", filePath)
@@ -658,7 +660,9 @@ func (m *Manager) BatchAddTagsToFile(filePaths []string, tagIDs []string) error 
 	if err != nil {
 		return err
 	}
-	_ = tx.Rollback()
+	defer func() {
+		_ = tx.Rollback()
+	}()
 
 	for _, filePath := range filePaths {
 		for _, tagID := range tagIDs {
@@ -693,7 +697,9 @@ func (m *Manager) ClearAllTags() error {
 	if err != nil {
 		return err
 	}
-	_ = tx.Rollback()
+	defer func() {
+		_ = tx.Rollback()
+	}()
 
 	_, err = tx.Exec("DELETE FROM file_tags")
 	if err != nil {
