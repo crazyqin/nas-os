3332b63046a4141d1ff7d84c20d6f7995189f0e4
crazyqin <crazyqin@users.noreply.github.com>
2026-03-14 07:40:12 +0800

    chore: 添加版本信息和 CHANGELOG

---
+++ internal/version/version.go
@@ -0,0 +1,32 @@
+// Package version provides version information for NAS-OS
+package version
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Version information
+var (
+	Version   = "2.5.0"
+	BuildDate = ""
+	GitCommit = ""
+	GoVersion = runtime.Version()
+	Platform  = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
+)
+
+// Info returns version information
+func Info() map[string]string {
+	return map[string]string{
+		"version":    Version,
+		"build_date": BuildDate,
+		"git_commit": GitCommit,
+		"go_version": GoVersion,
+		"platform":   Platform,
+	}
+}
+
+// String returns version string
+func String() string {
+	return fmt.Sprintf("NAS-OS v%s (%s)", Version, Platform)
+}
