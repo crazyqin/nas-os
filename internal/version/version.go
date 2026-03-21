34051ea30be258b501b61be4b5a9a5e2b8fee2b2
crazyqin <crazyqin@users.noreply.github.com>
2026-03-21 10:48:08 +0800

    chore: release v2.253.104
    - 版本号更新至 v2.253.104
    - 更新 CHANGELOG.md
    - 更新 README.md 版本号
    - 更新 MILESTONES.md
    - 六部协同例行维护

--- internal/version/version.go
+++ internal/version/version.go
@@ -8,8 +8,8 @@ import (
 
 // Version information
 var (
-	Version   = "2.253.103"
-	BuildDate = ""
+	Version   = "2.253.104"
+	BuildDate = "2026-03-21"
 	GitCommit = ""
 	GoVersion = runtime.Version()
 	Platform  = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
