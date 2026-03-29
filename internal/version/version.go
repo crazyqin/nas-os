aa656cd3f0584ad97fa794a27db78a0038d7353b
crazyqin <crazyqin@users.noreply.github.com>
2026-03-29 10:04:54 +0800

    feat(v2.306.0): 六部协同开发第82轮 - 竞品学习深化

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,9 +2,9 @@ package version
 
 // Version information
 const (
-	Version   = "2.304.0"
+	Version   = "2.306.0"
 	BuildDate = "2026-03-29"
-	GitCommit = "v2.304.0"
+	GitCommit = "v2.306.0"
 )
 
 // GetVersion returns the current version
