81a233334f63f983ad3112a2896bed0a6d83c6db
crazyqin <crazyqin@users.noreply.github.com>
2026-03-30 00:13:30 +0800

    吏部版本管理 - 版本号更新至v2.317.0 (第92轮)

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,9 +2,9 @@ package version
 
 // Version information
 const (
-	Version   = "2.315.0"
-	BuildDate = "2026-03-29"
-	GitCommit = "v2.315.0"
+	Version   = "2.317.0"
+	BuildDate = "2026-03-30"
+	GitCommit = "v2.317.0"
 )
 
 // GetVersion returns the current version
