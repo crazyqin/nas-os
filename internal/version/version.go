2ff5ca1b71d28b9ffde09bc7c57741ae585675aa
crazyqin <crazyqin@users.noreply.github.com>
2026-04-08 23:09:44 +0800

    feat: 第199轮六部协同开发完成；WebUI现代化优化；HA测试修复；版本v2.430.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,9 +2,9 @@ package version
 
 // Version information
 const (
-	Version   = "2.429.0"
-	BuildDate = "2026-04-08"
-	GitCommit = "v2.429.0"
+	Version   = "2.430.0"
+	BuildDate = "2026-04-09"
+	GitCommit = "v2.430.0"
 )
 
 // GetVersion returns the current version
