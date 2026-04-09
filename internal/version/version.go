9025b7e80d0e81772bc4cbe4a2ddf43df0b8dd1c
crazyqin <crazyqin@users.noreply.github.com>
2026-04-09 22:08:54 +0800

    [司礼监] 第211轮六部协同完成 - 安全审计Round211 + VERSION更新v2.439.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,9 +2,9 @@ package version
 
 // Version information
 const (
-	Version   = "2.434.0"
+	Version   = "2.438.0"
 	BuildDate = "2026-04-09"
-	GitCommit = "v2.434.0"
+	GitCommit = "v2.438.0"
 )
 
 // GetVersion returns the current version
