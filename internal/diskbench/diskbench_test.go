e0a91b6591cc8ae1e0765dee8cb4f79902dced7a
crazyqin <crazyqin@users.noreply.github.com>
2026-05-15 04:01:43 +0800

    feat: v2.490.35 - 兵部新增磁盘基准测试模块模块 - 顺序/随机读写测试

---
+++ internal/diskbench/diskbench_test.go
@@ -0,0 +1,34 @@
+package diskbench
+
+import (
+\t"testing"
+)
+
+func TestNewManager(t *testing.T) {
+\tcfg := &Config{Enabled: true, Interval: 60}
+\tm := NewManager(cfg)
+\tif m == nil {
+\t\tt.Fatal("NewManager returned nil")
+\t}
+}
+
+func TestManagerStartStop(t *testing.T) {
+\tcfg := &Config{Enabled: true, Interval: 60}
+\tm := NewManager(cfg)
+\t
+\terr := m.Start()
+\tif err != nil {
+\t\tt.Fatalf("Start failed: %v", err)
+\t}
+\tif !m.IsRunning() {
+\t\tt.Fatal("Manager should be running")
+\t}
+\t
+\terr = m.Stop()
+\tif err != nil {
+\t\tt.Fatalf("Stop failed: %v", err)
+\t}
+\tif m.IsRunning() {
+\t\tt.Fatal("Manager should be stopped")
+\t}
+}
